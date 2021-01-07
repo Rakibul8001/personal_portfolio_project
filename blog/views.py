3d9d2bb7133ccef45a8e8c7c3df227eba4929ae5
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2021-01-07 11:17:15 +0600

    blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
 
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+def detail(request,blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request,'blog/detail.html',{'blog_detail':blog_detail})
